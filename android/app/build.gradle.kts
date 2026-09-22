de35e9a437fc71b5f5137ad32e8614c7ca7a3136
vincentguerrerojrTLA <vincentguerrerojr@gmail.com>
2026-09-22 00:58:24 -0500

    Start Kylow Android Trust Core v1

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,9 @@
+plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
+
+android {
+    namespace = "com.kylow.mobile"
+    compileSdk = 35
+    defaultConfig { applicationId = "com.kylow.mobile"; minSdk = 26; targetSdk = 35; versionCode = 1; versionName = "0.1.0" }
+}
+
+dependencies { implementation("androidx.core:core-ktx:1.15.0"); implementation("androidx.appcompat:appcompat:1.7.0") }
