8c3b10f62e2de91f4c0d379f453f97934dcdf50b
vincentguerrerojrTLA <vincentguerrerojr@gmail.com>
2026-09-22 02:03:33 -0500

    Wire Kylow branded home screen

--- android/app/src/main/java/com/kylow/mobile/MainActivity.kt
+++ android/app/src/main/java/com/kylow/mobile/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.kylow.mobile
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
- override fun onCreate(savedInstanceState: Bundle?) { super.onCreate(savedInstanceState); setContentView(TextView(this).apply { text = "Kylow\nHe's just a good boy."; textSize = 24f; setPadding(48,96,48,48) }) }
+ override fun onCreate(savedInstanceState: Bundle?) {
+  super.onCreate(savedInstanceState)
+  setContentView(R.layout.activity_main)
+ }
 }
